0740c376b4cf657b5e01ecddd6193e59de19f26c
3aly <ali.hefny1998@gmail.com>
2024-03-07 14:55:22 +0200

    add import aliasing

---
+++ craco.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  webpack: {
+    alias: {
+      "@screens": path.resolve(__dirname, "src/screens"),
+      "@constants": path.resolve(__dirname, "src/constants"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@redux": path.resolve(__dirname, "src/redux"),
+      "@fakers": path.resolve(__dirname, "src/fakers"),
+      "@modals": path.resolve(__dirname, "src/modals"),
+      // Add other aliases as needed
+    },
+  },
+};
